80e80b0b44820d17db9ca40bbe7fa9bf68305173
TheDigimeister <thedigimeister@gmail.com>
2025-12-24 11:29:21 -0800

    leftMiddleGoal middle score timing

--- src/autons/LEFTMIDDLEGOAL.cpp
+++ src/autons/LEFTMIDDLEGOAL.cpp
@@ -13,21 +13,21 @@ void leftMiddleGoal(){
     matchload.set_value(true);
     chassis.turnToHeading(270, 500, {}, false);
     chassis.setPose(positionFromRaycast(front_dist.get() * MM_TO_IN, FRONT_DIST_OFFSET, WEST), positionFromRaycast(right_dist.get() * MM_TO_IN, RIGHT_DIST_OFFSET, NORTH),chassis.getPose().theta);
-    chassis.moveToPoint(-65, 48, 800, {.forwards=true, .maxSpeed=45, .minSpeed=45}, true);
-    pros::delay(800);
+    chassis.moveToPoint(-65, 48, 700, {.forwards=true, .maxSpeed=45, .minSpeed=45}, true);
+    pros::delay(700);
     chassis.turnToHeading(270, 500, {}, false);
     chassis.setPose(positionFromRaycast(front_dist.get() * MM_TO_IN, FRONT_DIST_OFFSET, WEST), positionFromRaycast(right_dist.get() * MM_TO_IN, RIGHT_DIST_OFFSET, NORTH),chassis.getPose().theta);
     // chassis.turnToPoint(-10, 10, 1000, {.forwards=false, .maxSpeed=127, .minSpeed=5, .earlyExitRange=5}, true);
     chassis.moveToPose(-9, 8, 320, 2400, {.forwards=false, .maxSpeed=127}, false);
     matchload.set_value(false);
     triple_state = MIDDLESCORE;
-    pros::delay(3000);
+    pros::delay(2000);
     triple_state = TICKLER;
     chassis.turnToPoint(-47, 47, 1000, {.forwards=true, .maxSpeed=127, .minSpeed=5, .earlyExitRange=5}, true);
     chassis.moveToPoint(-47, 47, 2000, {.forwards=true, .maxSpeed=127}, true);
     chassis.turnToHeading(270, 1000, {}, false);
     chassis.setPose(positionFromRaycast(front_dist.get() * MM_TO_IN, FRONT_DIST_OFFSET, WEST), positionFromRaycast(right_dist.get() * MM_TO_IN, RIGHT_DIST_OFFSET, NORTH),chassis.getPose().theta);
-    chassis.moveToPoint(-24, 47, 2000, {.forwards=false, .maxSpeed=50}, false);
+    chassis.moveToPoint(-24, 48, 1000, {.forwards=false, .maxSpeed=50}, false);
     triple_state = LONGSCORE;
     pros::delay(800);
 
