8ed52a7da79c06530a39095f0c7a32df0cd2ce98
jihojason <dxu20110714@gmail.com>
2025-12-24 11:42:13 -0800

    dih

--- src/autons/LEFT7BALL.cpp
+++ src/autons/LEFT7BALL.cpp
@@ -15,39 +15,40 @@ void left7ball() {
 
     chassis.moveToPoint(-21, 25, 250, {.maxSpeed=127, .minSpeed = 60, .earlyExitRange = 36});
     chassis.moveToPoint(-21, 25, 2000, {.maxSpeed = 60, .minSpeed = 60, .earlyExitRange = 8});
-    pros::delay(500);
-    chassis.turnToPoint(-45, 48, 500);
+    pros::delay(450);
+    chassis.turnToPoint(-54, 47, 1000);
     // pick up long goal balls
 
     // back up
-    chassis.moveToPoint(-45, 48, 1500, {.forwards=true, .maxSpeed=127, .minSpeed=5, .earlyExitRange=1});
+    chassis.moveToPoint(-54, 47, 800, {.forwards=true, .maxSpeed=127});
+    chassis.moveToPoint(-54, 47, 800, {.forwards=true, .maxSpeed=80});
     matchload.set_value(true);
     chassis.turnToHeading(270, 500, {}, false);
-    chassis.setPose(positionFromRaycast(front_dist.get()*MM_TO_IN, FRONT_DIST_OFFSET, WEST), positionFromRaycast(right_dist.get()*MM_TO_IN, RIGHT_DIST_OFFSET, NORTH), chassis.getPose().theta);
+    chassis.setPose(chassis.getPose().x, positionFromRaycast(right_dist.get()*MM_TO_IN, RIGHT_DIST_OFFSET, NORTH), chassis.getPose().theta);
 
-    chassis.moveToPoint(-61, 48, 900, {.forwards=true, .maxSpeed=127, .minSpeed = 45, .earlyExitRange=12});
-    chassis.moveToPoint(-61, 48, 800, {.forwards=true, .maxSpeed=45, .minSpeed = 45});
-    pros::delay(850);
+    chassis.moveToPoint(-63, 48, 900, {.forwards=true, .maxSpeed=127, .minSpeed = 45, .earlyExitRange=12});
+    chassis.moveToPoint(-66, 48, 700, {.forwards=true, .maxSpeed=45, .minSpeed = 45});
+    pros::delay(600);
    
     // move to goals
-        
-    chassis.moveToPoint(-24, 48.5, 2000, {.forwards=false, .maxSpeed=127});
-    chassis.waitUntil(5);
+    
+    chassis.turnToHeading(270, 500);
+    chassis.setPose(chassis.getPose().x, positionFromRaycast(right_dist.get()*MM_TO_IN, RIGHT_DIST_OFFSET, NORTH), chassis.getPose().theta);
+    chassis.moveToPose(-24, 48.5, 270, 2000, {.forwards=false, .maxSpeed=127});
     pros::Task align_score2{[=]{
-        while(back_dist.get() > 70) { pros::delay(50);}
+        while(back_dist.get() > 200) { pros::delay(50);}
+        triple_state = LONGSCORE;
     }};
-    triple_state = LONGSCORE;
-    pros::delay(2800);
+    pros::delay(2900);
 
     // descore
     matchload.set_value(false);
-    chassis.moveToPoint(-46, 37.5, 1500, {.minSpeed=5, .earlyExitRange=1.5});
+    chassis.moveToPoint(-46, 62, 1500, {.minSpeed=5, .earlyExitRange=1.5});
     // descore.set_value(true);
-    chassis.turnToHeading(90, 700, {.minSpeed=5, .earlyExitRange=1.25});
-    chassis.moveToPoint(-11, 37.5, 2000, {.forwards=true, .minSpeed = 30, .earlyExitRange = 3});
+    chassis.turnToHeading(270, 700, {.minSpeed=5, .earlyExitRange=1.25});
+    chassis.moveToPoint(-12, 62, 2000, {.forwards=false, .maxSpeed=90, .minSpeed = 30, .earlyExitRange = 3});
     chassis.waitUntil(15.2);
     // descore.set_value(false);
-    chassis.turnToHeading(120, 2000);
 
     
 
--- src/autons/RIGHT7BALL.cpp
+++ src/autons/RIGHT7BALL.cpp
@@ -4,56 +4,51 @@
 #include "utils.hpp"
 
 void right7ball() {
-    // chassis.setPose(-46.847,-14.278,90);
+        // chassis.setPose(-46.847,-14.278,90);
     chassis.setPose(positionFromRaycast(back_dist.get() * MM_TO_IN, BACK_DIST_OFFSET, WEST), positionFromRaycast(right_dist.get() * MM_TO_IN, RIGHT_DIST_OFFSET, SOUTH),90);
     // chassis.setPose()
 
 
-    // 7-ball low goal side (31)
-
+    // 9-ball low goal side (37)
+    triple_state = TICKLER;
     intake.move(127);
 
     // pick up trio
 
-    chassis.moveToPoint(-22, -26, 250, {.maxSpeed=127, .minSpeed = 60, .earlyExitRange = 36});
-    chassis.moveToPoint(-22, -26, 2000, {.maxSpeed = 60, .minSpeed = 60, .earlyExitRange = 4});
-    pros::delay(500);
-    chassis.turnToPoint(-45, -48, 500);
-
+    chassis.moveToPoint(-21, -25, 250, {.maxSpeed=127, .minSpeed = 60, .earlyExitRange = 36});
+    chassis.moveToPoint(-21, -25, 2000, {.maxSpeed = 60, .minSpeed = 60, .earlyExitRange = 8});
+    pros::delay(450);
+    chassis.turnToPoint(-54, -47, 1000);
+    // pick up long goal balls
 
     // back up
-    chassis.moveToPoint(-45, -49, 1000);
+    chassis.moveToPoint(-54, -47, 800, {.forwards=true, .maxSpeed=127});
+    chassis.moveToPoint(-54, -47, 800, {.forwards=true, .maxSpeed=80});
     matchload.set_value(true);
     chassis.turnToHeading(270, 500, {}, false);
-    chassis.setPose(positionFromRaycast(front_dist.get()*MM_TO_IN, FRONT_DIST_OFFSET, WEST), positionFromRaycast(left_dist.get()*MM_TO_IN, LEFT_DIST_OFFSET, SOUTH), chassis.getPose().theta);
+    chassis.setPose(chassis.getPose().x, positionFromRaycast(left_dist.get()*MM_TO_IN, LEFT_DIST_OFFSET, SOUTH), chassis.getPose().theta);
 
-    chassis.moveToPoint(-61, -48, 900, {.forwards=true, .maxSpeed=127, .minSpeed = 45, .earlyExitRange=12});
-    chassis.moveToPoint(-61, -48, 800, {.forwards=true, .maxSpeed=45, .minSpeed = 45});
-    pros::delay(800);
-    
-    
+    chassis.moveToPoint(-63, -48, 900, {.forwards=true, .maxSpeed=127, .minSpeed = 45, .earlyExitRange=12});
+    chassis.moveToPoint(-66, -48, 700, {.forwards=true, .maxSpeed=45, .minSpeed = 45});
+    pros::delay(600);
+   
     // move to goals
-    intake.move(127);
-        
-    chassis.moveToPoint(-24, -48.5, 2000, {.forwards=false, .maxSpeed=127});
-    chassis.waitUntil(5);
-    pros::Task align_score2{[=]{
-        while(back_dist.get() > 70) { pros::delay(50);}
-    }};
     
-    pros::delay(3000);
+    chassis.turnToHeading(270, 500);
+    chassis.setPose(chassis.getPose().x, positionFromRaycast(left_dist.get()*MM_TO_IN, LEFT_DIST_OFFSET, SOUTH), chassis.getPose().theta);
+    chassis.moveToPose(-24, -48.5, 270, 2000, {.forwards=false, .maxSpeed=127});
+    pros::Task align_score2{[=]{
+        while(back_dist.get() > 200) { pros::delay(50);}
+        triple_state = LONGSCORE;
+    }};
+    pros::delay(2900);
 
     // descore
-    chassis.moveToPoint(-46, -37.5, 1500, {.minSpeed=5, .earlyExitRange=1.5});
-    // descore.set_value(true);
-    chassis.turnToHeading(-90, 1000, {.minSpeed=5, .earlyExitRange=1.25});
-    chassis.moveToPoint(-17.5, -36.5, 2000, {.forwards=false, .minSpeed = 30, .earlyExitRange = 3});
-    chassis.waitUntil(15);
-    // descore.set_value(false);
-    chassis.turnToHeading(-120, 2000);
-
     matchload.set_value(false);
-
-
-
+    chassis.moveToPoint(-46, 38, 1500, {.minSpeed=5, .earlyExitRange=1.5});
+    // descore.set_value(true);
+    chassis.turnToHeading(90, 700, {.minSpeed=5, .earlyExitRange=1.25});
+    chassis.moveToPoint(-12, 38, 2000, {.forwards=true, .maxSpeed=90, .minSpeed = 30, .earlyExitRange = 3});
+    chassis.waitUntil(15.2);
+    // descore.set_value(false);
 }
